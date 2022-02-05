7fd1d4981adc743c010a44c11d58612d7ddea928
Andreas Frömer <andreas.froemer@check24.de>
2022-02-05 13:10:26 +0100

    test

--- scoper.inc.php
+++ scoper.inc.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Isolated\Symfony\Component\Finder\Finder;
 
 return [
-    'prefix' => 'ComposerUnused',
+    'prefix' => '_ComposerUnused_',
 
     // By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
     // directory. You can however define which files should be scoped by defining a collection of Finders in the
@@ -44,5 +44,6 @@ return [
     //
     // Fore more see https://github.com/humbug/php-scoper#whitelist
     'whitelist' => [
+        "Symfony\*"
     ],
 ];
